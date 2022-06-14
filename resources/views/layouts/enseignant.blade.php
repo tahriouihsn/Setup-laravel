a180d3ab51d1f30d4f03e3b5a3d3f13146668517
Your Name <tahriouihsn>
2022-06-14 22:05:38 +0100

    changes

--- resources/views/layouts/enseignant.blade.php
+++ resources/views/layouts/enseignant.blade.php
@@ -1,5 +1,5 @@
 
- 
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -11,24 +11,25 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="{{asset('enseignant/css/style.css')}}">
-    <title>prof</title>
+    <title>Enseignant</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
-        <a class="navbar-brand" href="#"><img  class="img-fluid" src="{{asset('enseignant/images/enset2.png')}}"  alt="ENSET"></a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class=" d-flex align-items-center mb-0" style="gap:8px ;""   href="/Prof"><img  class="img-fluid" style="max-height:60px" src="{{asset('enseignant/images/logofsr.png')}}"  alt="ENSET"> <span class="text-white font-weight-bold" style="font-size:18px;">UM5</span></a>
+
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-        
+
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
                 <a class="nav-link" href="{{ route('home.prof')}}">Accueil <span class="sr-only">(current)</span></a>
-              </li>   
+              </li>
 
                <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  Servises
+                  Services
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                   <a class="dropdown-item" href="{{ route('create.seance')}}">Créer une séance </a>
@@ -39,11 +40,11 @@
           </ul>
           <ul class="navbar-nav">
             <li class="nav-item">
-           
+
             <a class=" btn btn-primary" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        Deconnecter 
+                                        Déconnecter
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
@@ -51,23 +52,23 @@
                                     </form>
           </li>
           </ul>
-              
+
           </div>
         </nav>
-   
+
 
         @yield('content')
 
-  
+
     <section class="footer text-center">
       <div class="container">
         <div class="row">
-          <div class="col-md-6  ">
+          <div class="col-md-6  text-left">
                   <h3>SiteMap</h3>
                   <ul class="list-unstyled  three-columns">
                     <li><a href="#">Accueil</a></li>
                     <li><a href="#">Contact</a></li>
-                    <li><a href="#">Servises</a></li>
+                    <li><a href="#">Services</a></li>
                   </ul>
                   <ul class='list-unstyled sosial'>
                     <li>  <i class="fa fa-google-plus-square fa-3x" aria-hidden="true"></i></li>
@@ -76,20 +77,26 @@
                     <li> <i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i></li>
                    </ul>
           </div>
-       
+
           <div class="col-md-6 ">
-            <h3>ENSET-M</h3>
-            <p class="lead"> 
-           Ecole Normale Supérieure de l'Enseignement Technique <br>
-           BP 159 Bd Hassan II Mohammedia <br>
-           Tél. : 05 23 32 22 20 - 05 23 32 35 30 <br>
-           Fax : 05 23 32 25 46
+            <div class="d-flex " style="gap:10px">
+            <img  class="img-fluid" style="max-height:60px" src="{{asset('enseignant/images/logofsr.png')}}"  alt="ENSET">
+            <h3>UM5</h3>
+            </div>
+            <p class=" text-left">
+          Faculté des sciences <br>
+           4 avenue Ibn Battouta     <br>
+           B.P. 1014 RP,Rabat <br>
+           <strong>Tél. :</strong>  +212 (0) 5 37 77 18 34 / 35 / 38 <br>
+            <strong>Fax :</strong>  +212 5 37 77 42 61 <br>
+            <strong>Email:</strong> decanat@fsr.ac.ma
+
             </p>
           </div>
-        </div> 
+        </div>
       </div>
       <div class="copyright text-center">
-        Copyright &copy;2020 <span>Gestion des absences</span>
+      <span>Gestion des absences</span>
       </div>
      </section>
 
